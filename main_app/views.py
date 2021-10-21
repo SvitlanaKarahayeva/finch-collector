4184d4593b8563e8bd3299a7eef3f4b2f0967116
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2021-10-21 19:03:12 -0400

    Adding full CRUD for finches, class based views

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # class Finch:
@@ -18,10 +19,25 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+# Finch views
 def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
-    
+
+
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    # success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
