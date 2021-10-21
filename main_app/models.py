d972fa4f4f976f41d58fd54efbb1cdde24836adf
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2021-10-21 14:47:47 -0400

    Adding model, detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    size = models.IntegerField()
+
+    def __int__(self):
+        return self.breed
