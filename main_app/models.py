6206e13e436140a15ad73c370efb231fcee613aa
Svitlana Karahayevae <svitlana.karahayeva@gmail.com>
2021-10-22 09:39:48 -0400

    Adding Feeding model- one-to many connection with finches(finch is one), function based view for add_feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,39 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 from django.urls import reverse
 
 # Create your models here.
+
+FOODS = (
+    (1, 'Nyjer Seed'),
+    (2, 'Sunflower Seeds'),
+    (3, 'White Proso Millet'),
+    (4, 'Rapeseed Seeds'),
+    (5, 'Canary Seeds'),
+    (6, 'Shelled And Cracked Corn')
+)
 
 class Finch(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     size = models.IntegerField()
 
-    def __int__(self):
+    def __str__(self):
         return self.breed
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={ 'finch_id': self.id })
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    food = IntegerField(
+        choices=FOODS,
+        default=FOODS[1][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_food_display()} on {self.date}'
+
+    class Meta:
+        ordering = ['-date']
